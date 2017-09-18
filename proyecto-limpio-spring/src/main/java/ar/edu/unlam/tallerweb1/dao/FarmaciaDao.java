712ff032b6286a71fddeaff8633425d3da9c7d0c
radamisil <radamisil@hotmail.com>
2017-09-17 21:51:10 -0300

    subo los métodos de consulta a la base de datos, Criterias dentro del DAO, falta verificar las correcciones de los profesores, para empezara chequear los JUNIT. ACLARACION: El FarmaciaDaoImpl, está roto todavía porque hay que ajustar cosas según lo que me respondan los profesores, por este motivo el punto 5 no va a compilar, vasa tener que comentar este.

--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/dao/FarmaciaDao.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/dao/FarmaciaDao.java
@@ -1,9 +1,17 @@
 package ar.edu.unlam.tallerweb1.dao;
 
+import ar.edu.unlam.tallerweb1.modelo.Barrio;
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
 import ar.edu.unlam.tallerweb1.modelo.Farmacia;
 
 public interface FarmaciaDao {
 	
 	Farmacia consultarFarmacia (Farmacia farmacia);
+
+	Farmacia consultarFarmaciaDiaMartes(Farmacia farmacia);
+
+	Farmacia farmaciasDeUnaCalle(Farmacia farmacia, Direccion direccion);
+
+	Farmacia farmaciasDeUnBarrio(Farmacia farmacia, Barrio barrio);
 
 }
--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/dao/FarmaciaDaoImpl.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/dao/FarmaciaDaoImpl.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import ar.edu.unlam.tallerweb1.modelo.Barrio;
+import ar.edu.unlam.tallerweb1.modelo.Direccion;
 import ar.edu.unlam.tallerweb1.modelo.Farmacia;
 
 @Repository("FarmaciaDao")
@@ -15,12 +17,37 @@ public class FarmaciaDaoImpl implements FarmaciaDao {
 	@Inject
     private SessionFactory sessionFactory;
 	
-	@Override
-	public Farmacia consultarFarmacia(Farmacia farmacia){
 	
-		final Session session = sessionFactory.getCurrentSession();
+	/*- Hacer con junit un test que busque todas las farmacias de turno los días martes.*/
+	@Override
+	public Farmacia consultarFarmaciaDiaMartes(Farmacia farmacia){	
+		final Session session = sessionFactory.getCurrentSession();		
 		return (Farmacia) session.createCriteria(Farmacia.class)
-				.add(Restrictions.eq("diaDeTurno", farmacia.getDiaDeTurno()))
+				.add(Restrictions.eq("nombre", farmacia.getNombre()))
+				.add(Restrictions.eq("martes", farmacia.getDiaDeTurno()))
 				.list();		
-    }
+    }	
+		
+		
+	/*Hacer con junit un test que busque todas las farmacias de una calle.*/
+	@Override
+	public Farmacia farmaciasDeUnaCalle(Farmacia farmacia, Direccion direccion){
+		final Session session = sessionFactory.getCurrentSession();
+		return (Farmacia) session.createCriteria(Farmacia.class)    
+				.add(Restrictions.eq("nombre", farmacia.getNombre()))
+			        .add(Restrictions.eq("nombre", Direccion))
+				.list();
+	
+	}
+
+	
+	/*Hacer con junit un test que busque todas las farmacias de un barrio*/
+	@Override
+	public Farmacia farmaciasDeUnBarrio(Farmacia farmacia, Barrio barrio){
+		final Session session = sessionFactory.getCurrentSession();
+		return (Farmacia) session.createCriteria(Farmacia.class)    
+				.add(Restrictions.eq("nombre", farmacia.getNombre()))
+				.add(Restrictions.eq("nombre", Barrio))
+				.list();
+	}
 }
