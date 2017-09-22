ad87a44b34125c18147f05b4a5c577e8800f74a3
Diego <diegoalerei@gmail.com>
2017-09-21 21:05:06 -0300

    Punto 5.
    Se vuelve a commitear para tener una version definitiva

--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorFarmacia.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorFarmacia.java
@@ -1,5 +1,11 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import javax.inject.Inject;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +16,21 @@ import ar.edu.unlam.tallerweb1.modelo.Farmacia;
 
 @Controller
 public class ControladorFarmacia {
+	@Inject
+    private SessionFactory sessionFactory;
 	
 	@RequestMapping("/Farmacia")
 	
 	public ModelAndView Farmacia(){
 		
+		
 		ModelMap modelo = new ModelMap();
 		Farmacia Farmacia = new Farmacia();
+		
+		/*final Session session = sessionFactory.getCurrentSession();		
+		 Criteria cr = (Criteria) session.createCriteria(Farmacia.class)
+				.add(Restrictions.eq("DiaDeTurno", "martes"))				
+				.list();*/
 		
 		modelo.put("Farmacia", Farmacia);
 		return new ModelAndView("Farmacia", modelo);
--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/modelo/Direccion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ar.edu.unlam.tallerweb1.modelo;
 
 import javax.persistence.Entity;
@@ -54,60 +53,3 @@ public class Direccion {
 	}
 	
 }
-=======
-package ar.edu.unlam.tallerweb1.modelo;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-@Entity
-public class Direccion {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String calle;
-	private String numero;
-	
-	@ManyToOne
-	private Barrio barrio;
-	
-	public Direccion() {
-	}
-
-	public Direccion(String calle,String numero, Barrio barrio) {
-		this.calle = calle;
-		this.numero = numero;
-		this.barrio = barrio;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getCalle() {
-		return calle;
-	}
-	public void setCalle(String calle) {
-		this.calle = calle;
-	}
-	public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public Barrio getBarrio() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-}
->>>>>>> origin/master
